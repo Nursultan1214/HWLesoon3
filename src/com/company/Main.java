d8fee2c0d30ab28727546d0a9f47b08f20684ca0
Nursultan1214 <Nurchik1214@mail.ru>
2021-04-25 12:08:33 +0600

    ver2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,14 @@
 package com.company;
 
 import javax.imageio.metadata.IIOMetadataFormatImpl;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        double[] nums = {10.1, -5.6, -8.9, 11.2, 12.3, -6.6, -2.3, 5.6, 7.1, 9.1, 6.8, 8.8, -9.6, 13.4, 14.5};
-        //double [] nums = {8, -2, -4, 2, 3, 6};
+       // double[] nums = {10.1, -5.6, -8.9, 11.2, 12.3, -6.6, -2.3, 5.6, 7.1, 9.1, 6.8, 8.8, -9.6, 13.4, 14.5};
+        double [] nums = {8, -2, -4, 2, 3, 6};
         boolean isNegative = false;
         double summ = 0;
         double count = 0;
@@ -24,5 +25,21 @@ public class Main {
 
         System.out.println("ответ " +(summ / count));
 
+
+            // доп ДЗ
+
+        //double [] nums = {8, -2, -4, 2, 3, 6};
+
+        for (int i = nums.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j+1]){
+                    double temp = nums[j];
+                    nums [j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+
+            }
+            System.out.println("Отсортированный список: " + Arrays.toString(nums));
+        }
     }
 }
